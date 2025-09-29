0e43fe2708be238c18f220fe4984cc753bb6c5f0
a-n-a-s <mirzaanas815@gmail.com>
2025-09-29 09:38:17 +0530

    auth and dash side

--- backend/config.py
+++ backend/config.py
@@ -4,5 +4,3 @@ MONGO_URI = os.getenv("MONGO_URI", "mongodb://localhost:27017")
 JWT_SECRET = os.getenv("JWT_SECRET", "supersecret")
 JWT_ALGORITHM = "HS256"
 
-GOOGLE_CLIENT_ID = os.getenv("GOOGLE_CLIENT_ID","841150833613-eqa1injo302iou7f62nr5jsau402ospv.apps.googleusercontent.com")
-GOOGLE_CLIENT_SECRET = os.getenv("GOOGLE_CLIENT_SECRET", "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")
